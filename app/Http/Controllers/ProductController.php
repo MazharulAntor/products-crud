788ccc812143ef3d4ca2798577db1535c6cccf6d
Md Mazharul Islam Antor <mazharulantor2015@gmail.com>
2023-04-20 03:08:28 +0600

    Add product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function index(){
-        $products = "Product List from ProductController!";
+    public function index()
+    {
+        $products = "Products List from ProductController!";
         return view('products.index', ['products' => $products]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $product = new Product;
+
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+        request()->image->move(public_path('images'), $file_name);
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->image = $file_name;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+
+        $product->save();
+        return redirect()->route('products.index')->with('success', 'Product Added Successfully');
     }
 }
